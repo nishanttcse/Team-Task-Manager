e01955143351f17f575c6018c1cc74afc3b11993
Nishant <nishantgenius03@gmail.com>
2026-05-01 08:48:23 +0530

    fix role middleware

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createProject, getProjects } from "../controllers/projectcontroller.js";
-import { protect, isAdmin } from "../middleware/auth.js";
+import { protect } from "../middleware/auth.js";
+import { authorizeRoles } from "../middleware/role.js";
 
 const router = express.Router();
 
-router.post("/", protect, isAdmin, createProject);
+router.post("/", protect, authorizeRoles("admin"), createProject);
 router.get("/", protect, getProjects);
 
 export default router;
